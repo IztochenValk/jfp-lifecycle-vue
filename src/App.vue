f015c8d414a8aa15f0d899a419d9df23d838f729
Florian Chague <florian.chague2@gmail.com>
2025-10-30 14:48:10 +0100

    lololol

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,52 @@
 <template>
-  <PokemonBadge />
-  <Meteo />
+  <MessageCard v-for="ami in lesAmis"  v-bind="ami"/>
 </template>
 
 <script setup>
-import PokemonBadge from './components/PokemonBadge.vue';
-import Meteo from './components/Meteo.vue';
+import MessageCard from './components/MessageCard.vue';
+import { ref } from 'vue'
+const lesAmis = ref([
+    {
+        id: 'lasticot',
+        name: 'COCO L ASTICOT',
+        phone: '01234 5678 991',
+        email: 'coco@lasticot.com',
+        premium: true
+    },
+    {
+        id: 'lasticot2',
+        name: 'COCO L ASTICOT',
+        phone: '01234 5678 991',
+        email: 'coco@lasticot.com',
+        premium: true
+    },
+    {
+        id: 'kimonoSurUnFrigo',
+        name: "Steven Seagal",
+        phone: '+338765477',
+        email: 'steven@seagal.com',
+        premium: true
+    },
+    {
+        id: 'yoyoyo',
+        name: "JAROD",
+        phone: '+338765477',
+        email: 'jAROD@seagal.com',
+        premium: false
+    },
+    {
+        id: 'yoyoyo',
+        name: "JAROD",
+        phone: '+338765477',
+        email: 'jAROD@seagal.com',
+        premium: true
+    },
+    {
+        id: 'GUS',
+        name: "GussDX",
+        phone: '+338765477',
+        email: 'jAROD@seagal.com',
+        premium: false
+    }
+]);
 </script>
---
+++ src/components/MessageCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card w-full max-w-lg shadow-xl">
+    <div class="card-body">
+      <h5 class="card-title text-base-content">👤 {{ name }}</h5>
+      <div class="text-base-content opacity-80">🆔 {{ id }}</div>
+      <div class="text-base-content opacity-80">📧 {{ email }}</div>
+      <div class="text-base-content opacity-80">📱 {{ phone }}</div>
+      <div class="text-base-content opacity-80">
+        <div v-if="premium == true">
+            un ami en or 💎ᴠɪᴘ
+            <div class="badge badge-warning">premium</div>
+        </div>
+        <div v-else>
+            Un ami en Plomb 😞
+            <div class="badge">standard</div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name:  { type: String, required: true },
+  id:    { type: String, required: true },
+
+  email: {
+    type: String,
+    required: true,
+    validator: v => /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i.test(v)
+  },
+
+  phone: {
+    type: String,
+    required: true,
+    validator: v => /^((\+33\s?(?:\(0\)\s?)?)|0)[1-9](?:[ .-]?\d{2}){4}$/.test(v)
+  },
+
+  premium: { type: Boolean, default: false }
+})
+</script>
